81ac1d9af1020e022d106006323102c339c99707
dwijking2010 <dwij.king2010@gmail.com>
2024-01-02 09:54:40 +0000

    first commit

--- app.py
+++ app.py
@@ -1,38 +1,39 @@
-from flask import Flask, render_template, request
-
+#import flask library
+from flask import Flask,render_template,request
+#initialize flask
 app = Flask(__name__)
-
+#route your webpage
 @app.route("/")
 def visitors():
 
-    # Load current count
-    counter_read_file = open("count.txt", "r")
-    visitors_count = int(counter_read_file.read())
-    counter_read_file.close()
+	# Load current count
+	counter_read_file = open("count.txt", "r")
+	visitors_count = int(counter_read_file.read())
+	counter_read_file.close()
 
-    # Increment the count
-    visitors_count = visitors_count + 1
+	# Increment the count
+	visitors_count = visitors_count + 1
 
-    # Overwrite the count
-    counter_write_file = open("count.txt", "w")
-    counter_write_file.write(str(visitors_count))
-    counter_write_file.close()
+	# Overwrite the count
+	counter_write_file = open("count.txt", "w")
+	counter_write_file.write(str(visitors_count))
+	counter_write_file.close()
+	return render_template("index.html",count = visitors_count)
 
-    # Render HTML with count variable
-    return render_template("index.html", count=visitors_count)
-
-@app.route('/', methods=['POST'])
+# Render HTML with count variable
+@app.route('/',methods = ['POST'])
+#route your webpage
 def covid_stats():
-    # Load current count
-    counter_read_file = open("count.txt", "r")
-    visitors_count = int(counter_read_file.read())
-    counter_read_file.close()
-
-    text = request.form['text']
-
-    corona_data = 'https://corona.dnsforfamily.com/graph.png?c='+text
-    print(corona_data)
-    return render_template("index.html", image=corona_data, count=visitors_count)
-
-if __name__ == "__main__":
-    app.run()
+	# Load current count
+	counter_read_file = open("count.txt", "r")
+	visitors_count = int(counter_read_file.read())
+	counter_read_file.close()
+	text = request.form['text']
+	corona_data= 'https://covidstats-sdbd.onrender.com/?country='+text
+	return render_template("index.html",image = corona_data, count = visitors_count)
+    
+    
+	#complete the code
+if __name__=="__main__":
+	app.run()
+#add code for executing flask
